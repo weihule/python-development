8cc209805eccc6c845c387e7a7c94de40a55d827
weihule <weihule@outlook.com>
2024-01-31 19:41:07 +0800

    add some content

--- concurrent/test.py
+++ concurrent/test.py
@@ -2,19 +2,43 @@ from time import sleep
 import threading
 
 
+NUM = 0
+
+
 def task1():
-    while True:
-        print("sing ...")
-        sleep(1)
+    global NUM
+    NUM = 10
+    print(f"in task1 NUM = {NUM}")
+
 
 def task2():
-    while True:
-        print("dancing ...")
-        sleep(1.5)
+    print(f"in task2 NUM = {NUM}")
 
+
+def main1():
+    t1 = threading.Thread(target=task1)
+    t2 = threading.Thread(target=task2)
+    t1.start()
+    sleep(2)
+    t2.start()
+
+
+class MY(threading.Thread):
+    def run(self):
+        while True:
+            print(self.name)       # 查看当前线程的默认名字
+            print("11111")
+            sleep(1)
+
+
+def main2():
+    my = MY()
+    my.daemon = True  # 设置为守护线程
+    my.start()
+    while True:
+        print("main")
+        sleep(1)
+    
 
 if __name__ == "__main__":
-    t1 = threading.Thread(target=task1)
-    t2 = threading.Thread(target=task1)
-    t1.start()
-    t2.start()
+    main1()
